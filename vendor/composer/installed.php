f799f55b25ca288727ba96da1c291803997f0386
sava1978 <54743019+sava1978@users.noreply.github.com>
2023-11-16 00:03:51 +0000

    yolo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f1364b478a6f21fc493b9e20a4c6b983cd5b747e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f1364b478a6f21fc493b9e20a4c6b983cd5b747e',
+    ),
+  ),
+);
